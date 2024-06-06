0bcf4ca3e37327f8400195f8da2f6f0dffeab76b
pranayjoshi <pranayjoshi4466@gmail.com>
2024-06-06 14:36:32 -0500

    updated draft1 sourcegen

--- Draft1/SourceGen.cs
+++ Draft1/SourceGen.cs
@@ -1,6 +1,55 @@
-namespace JsonSchema.SourceGenerator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Text;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
 
-public class SourceGen
+namespace Corvus.Json.SchemaGenerator
 {
-    
+    [Generator]
+    public class JsonSchemaGenerator : IIncrementalGenerator
+    {
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            // Set up code generation pipeline
+            var providerProvider = context.SyntaxProvider
+                .CreateSyntaxProvider(
+                    (s, _) => IsSyntaxTargetForGeneration(s),
+                    (ctx, _) => GetSemanticTargetForGeneration(ctx))
+                .Where(static x => x is not null);
+
+            var compilation = context.CompilationProvider.Combine(providerProvider.Collect());
+
+            context.RegisterSourceOutput(compilation,
+                (spc, source) => Execute(source.Left, source.Right, spc));
+        }
+
+        private static bool IsSyntaxTargetForGeneration(SyntaxNode node)
+        {
+            // Determine if the syntax node should trigger code generation
+            return true;
+        }
+
+        private static bool GetSemanticTargetForGeneration(GeneratorSyntaxContext context)
+        {
+            // Perform semantic analysis and determine if code generation should be triggered
+            return true;
+        }
+
+        private static void Execute(Compilation compilation, ImmutableArray<ISymbol> symbols, SourceProductionContext context)
+        {
+            // Generate the source code and add it to the context
+            string sourceCode = GenerateSourceCode(compilation, symbols);
+            context.AddSource("GeneratedSource.cs", SourceText.From(sourceCode, Encoding.UTF8));
+        }
+
+        private static string GenerateSourceCode(Compilation compilation, ImmutableArray<ISymbol> symbols)
+        {
+            // Generate the source code based on the input compilation and symbols
+            return "// Generated code goes here";
+        }
+    }
 }
