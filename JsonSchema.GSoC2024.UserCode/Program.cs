0cb58b020982404bf842ac1ba7c8bc3f3df296e0
pranayjoshi <pranayjoshi4466@gmail.com>
2024-07-30 08:45:21 -0500

    Updated the UserCode Console app

--- JsonSchema.GSoC2024.UserCode/Program.cs
+++ JsonSchema.GSoC2024.UserCode/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+namespace JsonSchema.GSoC2024.UserCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+    [Generated("person.json")]
+public partial class Cl
+{
+}
+}
