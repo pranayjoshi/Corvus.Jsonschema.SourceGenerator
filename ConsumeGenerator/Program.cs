c0cdcccdc7af57ac100a54ffaa58f3ef358d18f3
pranayjoshi <pranayjoshi4466@gmail.com>
2024-08-20 16:32:47 -0500

    Final Updated SoruceGenerator Project

---
+++ ConsumeGenerator/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Corvus.Json;
+using SourceGenTest2.Model;
+
+FlimFlam flimFlam = JsonAny.ParseValue("[1,2,3]"u8);
+Console.WriteLine(flimFlam);
+JsonArray array = flimFlam.As<JsonArray>();
+Console.WriteLine(array);
